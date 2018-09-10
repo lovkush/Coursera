d4bbf5d3582a5a49695b3ab700c8caefa5252301
Lovkush <lovkushsingh15@gmail.com>
2018-09-11 04:27:12 +0530

    week1 assignment

---
+++ src/app/dish-comments/dish-comments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-comments',
+  templateUrl: './dish-comments.component.html',
+  styleUrls: ['./dish-comments.component.scss']
+})
+export class DishCommentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,7 @@
+export class dish{
+	name: string;
+	image : string;
+	category : string;
+	price: string;
+	label : string; 
+}
